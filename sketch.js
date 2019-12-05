e6259a35d4a0d5e286ac1c0219ea5c2af5c2b7c9
Adrien Duval <adrien.duval.bonnet@gmail.com>
2019-12-05 23:11:10 +0100

    Passage (foiré) aux complexes

--- sketch.js
+++ sketch.js
@@ -1,14 +1,15 @@
 var center = null, cercle = null, drawCircles = true;
-var centers = [], rayons = [], avancee = [], points = [];
-var zoom = 1, speed = 1;
+var centers = [], avancee = [], points = [];
+var zoom = 1, speed = 1, last;
 
 function setup() {
     createCanvas(windowWidth, windowHeight);
     frameRate(1000);
+    last = millis();
 }
 
 function draw() {
-    if (frameCount % parseInt(speed) == 0) background(0);
+    background(0);
 
     translate(width / 2, height / 2);
     scale(zoom, zoom);
@@ -17,47 +18,42 @@ function draw() {
     noFill();
     strokeWeight(1);
     stroke(255, 255, 255, 125);
-    let avancee = (10 * millis()) / (1000 * pow(2, centers.length));
-    let translateX = 0, translateY = 0;
-    for (let i = 0; i < centers.length; i++)
+    now = millis();
+    let step = (10 * (now - last)) / (1000 * pow(2, centers.length));
+    last = now;
+    for (let i = 1; i < centers.length; i++)
     {
-
-        if (i) {
-            var dx = cos(avancee) * rayons[i - 1];
-            var dy = sin(avancee) * rayons[i - 1];
-            translateX += dx
-            translateY += dy
-        }
-        else
-        {
-            translateX = centers[0].x;
-            translateY = centers[0].y;
-        }
-        push();
-        translate(translateX, translateY);
-
-        if (drawCircles && frameCount % parseInt(speed) == 0) {
-            strokeWeight(5);
-            point(0, 0);
-            if (typeof dx != "undefined") {
-                strokeWeight(3);
-                line(-dx, -dy, 0, 0);
-            }
-            strokeWeight(1);
-            ellipse(0, 0, rayons[i] * 2, rayons[i] * 2);
-        }
-
+        centers[i].argument += step; //avancee / frequence[i]
         avancee *= 1.7;
-        pop();
     }
 
-    if (centers.length) {
-        var tx = cos(avancee) * rayons[centers.length - 1];
-        var ty = sin(avancee) * rayons[centers.length - 1];
+    let translateX = 0, translateY = 0;
+    if (drawCircles && frameCount % parseInt(speed) == 0) {
+        for (let i = 0; i < centers.length - 1; i++)
+        {
+            centre = centers[i];
+            translateX += centre.getX();
+            translateY += centre.getY();
+            //push();
+            translate(translateX, translateY);
+
+            strokeWeight(5);
+            point(0, 0);
+            strokeWeight(3);
+            line(-centre.getX(), -centre.getY(), 0, 0);
+            strokeWeight(1);
+            ellipse(0, 0, centers[i + 1].module * 2, centers[i + 1].module * 2);
+            //pop();
+        }
+    }
+
+    /*if (centers.length) {
+        var tx = rayons[centers.length - 1].x;
+        var ty = rayons[centers.length - 1].y;
         points.push(createVector(translateX + tx, translateY + ty));
         strokeWeight(5);
         point(tx, ty);
-    }
+    }*/
 
     if (points.length > 2000)
         points = points.slice(1);
@@ -90,8 +86,6 @@ function draw() {
             line(c1.x, c1.y, c2.x, c2.y);
         }
     }
-
-    //filter(BLUR, 2);
 }
 
 function mouseWheel(e)
@@ -102,15 +96,12 @@ function mouseWheel(e)
         zoom -= 0.2;
 
     zoom = constrain(zoom, 0.2, 4);
-
-    //console.log(e, zoom);
 }
 
 function mouseMoved()
 {
     newCenter = createVector(mouseX, mouseY);
     cercle = [center, center.dist(newCenter)];
-    //center = newCenter;
 }
 
 function keyPressed()
@@ -131,8 +122,7 @@ function mouseReleased()
         center = createVector(mouseX, mouseY);
     else {
         newCenter = createVector(mouseX, mouseY);
-        centers.push(center);
-        rayons.push(center.dist(newCenter));
+        centers.push(cartesienVersComplexe(center));
         center = newCenter;
         points = [];
     }
